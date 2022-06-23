514048a8cab7c5910ead0f9c7c7f7b90376556ef
Mr. Zok3r <96907156+k4long666@users.noreply.github.com>
2022-06-23 21:08:53 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,11 +2,11 @@ import brute
 if __name__=='__main__':
 	try:os.system('git pull')
 	except:pass
-	try:os.system('touch .prox.txt')
+	try:os.system('touch .proxy.txt')
 	except:pass
 	try:os.mkdir('/sdcard/CP')
 	except:pass
-	try:os.mkdir('/sdcard/OK')
+	try:os.mkdir('/sdcard/LIVE')
 	except:pass
 	try:os.mkdir('/sdcard/DUMP')
 	except:pass
